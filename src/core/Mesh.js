92be300f5ff65c78af135ffe0c83d4edc27ad3bf
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-15 17:20:23 -0600

    feat: basic class init

--- src/core/Mesh.js
+++ src/core/Mesh.js
@@ -1,3 +1,11 @@
-export default class Mesh {
+import Object3D from "./Object3D";
 
+export default class Mesh extends Object3D {
+  constructor(gl, {
+    geometry,
+    shader,
+  }) {
+    this.geometry = geometry;
+    this.shader = shader;
+  }
 }
