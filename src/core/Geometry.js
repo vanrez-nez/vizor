1df9f695f1fd455241541f8d5722a3a297ea2f66
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-15 17:16:51 -0600

    feat: handling vertex buffer attributes

--- src/core/Geometry.js
+++ src/core/Geometry.js
@@ -1,1 +1,30 @@
-export default class Geometry {}
+import * as GL from '../const/GL';
+import { TypedArrToGLType } from '../utils/ArrayUtils';
+import VertexBufferAttribute from './VertexBufferAttribute';
+
+export default class Geometry {
+  constructor(gl) {
+    this.gl = gl;
+    this.vertices = [];
+    this.faces = [];
+    this.drawRange = { first: 0, count: 0 };
+    this.bufferAttributes = [];
+  }
+
+  setAttribute(name, { data, size, normalized, stride, offset }) {
+    const { drawRange, bufferAttributes, gl } = this;
+    if (!bufferAttributes[name]) {
+      const buff = new VertexBufferAttribute(gl, {
+        type: TypedArrToGLType(data),
+        usage: GL.STATIC_DRAW,
+        size,
+        normalized,
+        stride,
+        offset,
+      });
+      buff.setData(data);
+      drawRange.count = data.length / size;
+      bufferAttributes[name] = buff;
+    }
+  }
+}
--- src/utils/ArrayUtils.js
+++ src/utils/ArrayUtils.js
@@ -1,3 +1,5 @@
+import * as GL from '../const/GL';
+
 export function arraysEqual(a, b) {
   if (a === b) return true;
   if (a.length !== b.length) return false;
@@ -10,5 +12,21 @@ export function arraysEqual(a, b) {
 export function copyArray(from, to) {
   for(let i = 0; i < to.length; i++) {
     to[i] = from[i];
+  }
+  return to;
+}
+
+export function isArray(obj) {
+  return Array.isArray(obj) || ArrayBuffer.isView(obj) && !(obj instanceof DataView);
+}
+
+export function TypedArrToGLType(arr) {
+  switch(arr.constructor) {
+    case Float32Array: return GL.FLOAT;
+    case Uint16Array: return GL.UNSIGNED_SHORT;
+    case Uint32Array: return GL.UNSIGNED_INT;
+    case Int32Array: return GL.INT;
+    case Int16Array: return GL.SHORT;
+    case Int8Array: return GL.BYTE;
   }
 }
