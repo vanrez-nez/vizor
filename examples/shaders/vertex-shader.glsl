89053b2fc2a1a7433c599a12783834ffcab99cdf
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-15 17:17:55 -0600

    feat: triangle drawing

--- examples/shaders/vertex-shader.glsl
+++ examples/shaders/vertex-shader.glsl
@@ -1,5 +1,9 @@
-attribute vec4 position;
+attribute vec2 position;
+attribute vec2 uv;
+
+varying vec2 vUv;
 
 void main() {
-  gl_Position = position;
+  vUv = uv;
+  gl_Position = vec4(position, 0.0, 1.0);
 }
--- examples/test.js
+++ examples/test.js
@@ -1,19 +1,31 @@
 import vertexSource from './shaders/vertex-shader.glsl';
 import fragmentSource from './shaders/fragment-shader.glsl';
-import { Renderer, Shader } from '../src/index';
+import { DrawCall, Renderer, Shader, Mesh, Geometry } from '../src/index';
 
 class Test {
   constructor() {
     this.renderer = new Renderer('js-canvas');
-    this.renderer.resize(500, 500, true);
-    this.shader = new Shader(this.renderer.gl, {vertexSource, fragmentSource});
+    const { gl } = this.renderer;
+    this.shader = new Shader(gl, { vertexSource, fragmentSource });
+    this.geometry = new Geometry(gl);
+    this.geometry.setAttribute('position', { size: 2, data: new Float32Array([ -1, -1, 3, -1, -1, 3]) });
+    this.geometry.setAttribute('uv', { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) });
+    // this.shader.uniforms.u_color.value = [1, 1, 0];
+    this.drawCall = new DrawCall(this.renderer);
+    window.addEventListener('resize', this.resize.bind(this));
+    this.resize();
     this.render();
   }
 
+  resize() {
+    const { innerWidth, innerHeight } = window;
+    this.renderer.resize(innerWidth, innerHeight, true);
+  }
+
   render() {
-    const { renderer } = this;
-    renderer.clear();
-    renderer.render();
+    const { drawCall, geometry, shader } = this;
+    drawCall.draw(geometry, shader);
+    window.requestAnimationFrame(this.render.bind(this));
   }
 }
 
