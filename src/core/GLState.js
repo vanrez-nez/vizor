aeaab2ef2abe4f00c8cc9b3ed5be417987e2928d
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-15 17:16:07 -0600

    feat: viewport, program and buffer

--- src/core/GLState.js
+++ src/core/GLState.js
@@ -1,5 +1,46 @@
+import * as GL from '../const/GL';
+
 export default class GLState {
-  constructor() {
-    this.programId = -1;
+  constructor(gl) {
+    this.gl = gl;
+    this.program = -1;
+    this.viewport = { x: 0, y: 0, w: 0, h: 0 };
+    this.activeBuffer = -1;
+  }
+
+  useProgram(program) {
+    const { gl } = this;
+    if (this.program != program) {
+      gl.useProgram(program);
+      this.program = program;
+    }
+  }
+
+  setViewport(x, y, w, h) {
+    const { gl, viewport: v } = this;
+    if ( x != v.x || y != v.y || v.w != w || v.h != h) {
+      v.x = x;
+      v.y = y;
+      v.w = w;
+      v.h = h;
+      gl.viewport(x, y, w, h);
+    }
+  }
+
+  bindBuffer(target, buffer) {
+    const { gl, activeBuffer } = this;
+    if (activeBuffer !== buffer) {
+      gl.bindBuffer(target, buffer);
+      this.activeBuffer = buffer;
+      return true;
+    }
+    return false;
+  }
+
+  bufferData(target, data, usage) {
+    const { gl, activeBuffer } = this;
+    if (activeBuffer) {
+      gl.bufferData(target, data, usage);
+    }
   }
 }
