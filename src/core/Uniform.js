e58782ce7e97f7d25f0a066014b14d255f5b9aa9
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-17 17:20:11 -0600

    chore: formatting deconstruct and todo

--- src/core/Uniform.js
+++ src/core/Uniform.js
@@ -1,14 +1,22 @@
 import { getUniformSetter, getUniformDefaultValue } from '../utils/UniformUtils.js';
 import { arraysEqual, copyArray, isArray } from '../utils/ArrayUtils';
+import * as GL from '../const/GL';
 
 /*
   TODO: Optimize uniform setters by allowing to set by component.
   e.g: using uniform3f(loc, x, y, z) vs uniform3fv(loc, [x, y, z]);
   See: https://bit.ly/2S0muTQ IE and FF benchmarks are not conclusive
+  TODO: flat array when is matrix type before compare
 */
 
 export default class Uniform {
-  constructor(gl, { parts, name, type, location, size }) {
+  constructor(gl, {
+    parts,
+    name,
+    type,
+    location,
+    size
+  }) {
     this.gl = gl;
     this.type = type;
     this.name = name;
