e4812122dbced3b9c1bfc5b716ac742cb451743d
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-23 01:49:16 -0600

    feat: capabilities now containing limits and extensions

--- src/core/GLCapabilities.js
+++ src/core/GLCapabilities.js
@@ -1,32 +1,59 @@
-
 function toCamelCase(str) {
   return str.toLowerCase(str).replace(/(_.)/g, (match, chr) => {
     return chr[1].toUpperCase();
   });
 }
 
+function getLimits(gl) {
+  const cache = {};
+  const props = {};
+  for (let key in gl) {
+    if (/^MAX_/.test(key)) {
+      const name = toCamelCase(key);
+      props[name] = {
+        get() {
+          if (!cache[key]) {
+            cache[key] = gl.getParameter(gl[key]);
+          }
+          return cache[key];
+        }
+      };
+    }
+  }
+  return Object.defineProperties({}, props);
+}
+
+function getExtensions(gl) {
+  const cache = {};
+  const props = {};
+  const arr = gl.getSupportedExtensions();
+  for (let i = 0; i < arr.length; i++) {
+    const key = arr[i];
+    const name = toCamelCase(key);
+    props[name] = {
+      get() {
+        if (!cache[name]) {
+          cache[name] = gl.getExtension(key);
+        }
+        return cache[name];
+      }
+    };
+  }
+  return Object.defineProperties({}, props);
+}
+
+function getMaxAnisotropy(gl, extensions) {
+    const ext = extensions.extTextureFilterAnisotropic;
+    return ext ? gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT) : 0;
+}
+
 export default class GLCapabilities {
   constructor(gl) {
     this.webgl2 = gl instanceof WebGL2RenderingContext;
-    this.defineMaxProperties(gl);
-  }
-
-  defineMaxProperties(gl) {
-    const props = {};
-    const cache = {};
-    for (let key in gl) {
-      if (/^MAX_/.test(key)) {
-        const name = toCamelCase(key);
-        props[name] = {
-          get() {
-            if (!cache[key]) {
-              cache[key] = gl.getParameter(gl[key]);
-            }
-            return cache[key];
-          }
-        };
-      }
-    }
-    Object.defineProperties(this, props);
+    const extensions = getExtensions(gl);
+    const limits = getLimits(gl);
+    limits.maxAnisotropy = getMaxAnisotropy(gl, extensions);
+    this.extensions = extensions;
+    this.limits = limits;
   }
 }
