f4c940345063a8e12976cdb01b90e22ae85073b3
Iván Juárez Núñez <ivan@blindgazer.com>
2020-02-24 21:26:27 -0600

    refact: using promises to handle image load

--- src/loaders/ImageLoader.js
+++ src/loaders/ImageLoader.js
@@ -1,34 +1,27 @@
+
 export default class ImageLoader {
   constructor(src) {
     this.loading = false;
     this.loaded = false;
     this.image = new Image();
     this.src = src;
-    this.listeners = [];
-    this.load();
-  }
-
-  notify() {
-    this.listeners.forEach(listener => listener(this.image));
-    this.listeners = [];
-  }
-
-  onDone(callback) {
-    if (this.loaded) {
-      callback(this.image);
-    } else {
-      this.listeners.push(callback);
-    }
   }
 
   load() {
     const { image, src } = this;
-    this.loading = true;
-    image.onload = () => {
-      this.loading = false;
-      this.loaded = true;
-      this.notify();
-    }
+    const promise = new Promise(resolve => {
+      if (this.loaded) {
+        resolve(image);
+      } else {
+        this.loading = true;
+        image.onload = () => {
+          this.loading = false;
+          this.loaded = true;
+          resolve(image);
+        }
+      }
+    });
     image.src = src;
+    return promise;
   }
 }
