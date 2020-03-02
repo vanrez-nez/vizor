b8ae3b8a58298dfb64b7ce2dc13ecf5cdf14b145
Iván Juárez Núñez <ivan@blindgazer.com>
2020-03-01 21:11:41 -0600

    feat: common math utils

---
+++ src/utils/MathUtils.js
@@ -0,0 +1,29 @@
+// https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
+
+export function isPowerOfTwo(x) {
+  return (x & (x - 1)) == 0;
+}
+
+export function floorPowerOfTwo(x) {
+  let p = 1;
+  while( x >>= 1) p <<= 1;
+  return p;
+}
+
+export function ceilPowerOfTwo(x) {
+  if (x < 1) return 1;
+  let p = 2;
+  x--;
+  while( x >>= 1) p <<= 1;
+  return p;
+}
+
+export function getSizeToCover(width, height, maxWidth, maxHeight) {
+  var ratio = Math.max(maxWidth / width, maxHeight / height);
+  return [width * ratio, height * ratio];
+}
+
+export function getSizeToContain(width, height, maxWidth, maxHeight) {
+  var ratio = Math.min(maxWidth / width, maxHeight / height);
+  return [width * ratio, height * ratio];
+}
