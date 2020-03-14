5c28573c91f260950650aead66d1a40064266c5d
Iván Juárez Núñez <ivan@blindgazer.com>
2020-03-13 22:12:32 -0600

    chore: camera, scene and math utils exports

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ export { default as Renderer } from './core/Renderer';
 export { default as DrawCall } from './core/DrawCall';
 export { default as Texture2D } from './core/Texture2D';
 export { default as Shader } from './core/Shader';
+export { default as Scene } from './core/Scene';
+export { default as Camera } from './core/Camera';
 export { default as Mesh } from './core/Mesh';
+export { default as Euler } from './math/Euler';
+export { default as Quat } from './math/Quat';
 export { default as Vec4 } from './math/Vec4';
 export { default as Vec3 } from './math/Vec3';
 export { default as Vec2 } from './math/Vec2';
+export { default as Mat3 } from './math/Mat3';
+export { default as Mat4 } from './math/Mat4';
 export { default as ImageLoader } from './loaders/ImageLoader';
-
 export { default as Addons } from './core/Addons';
 export { default as TextureResize } from './addons/TextureResize';
 export { default as ContextHandler } from './addons/ContextHandler';
+
+
+import * as MathUtils from './utils/MathUtils';
+
+export { MathUtils };
